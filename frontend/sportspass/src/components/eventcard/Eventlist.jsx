89688b6fc638b74b0911481c05a965d5384f1136
unknown <bijoordzen@gmail.com>
2024-05-24 00:32:42 +0530

    fixed events list

--- frontend/sportspass/src/components/eventcard/Eventlist.jsx
+++ frontend/sportspass/src/components/eventcard/Eventlist.jsx
@@ -6,14 +6,12 @@ const EventList = () => {
     
     return(
         <div>
-            <Eventcard title ={events[0].title} thumbnail ={events[0].thumbnail} date ={events[0].date} location ={events[0].location} description = {events[0].description} />
-            <Eventcard title ={events[1].title} thumbnail ={events[1].thumbnail} date ={events[1].date} location ={events[1].location} description = {events[1].description} />
-            <Eventcard title ={events[2].title} thumbnail ={events[2].thumbnail} date ={events[2].date} location ={events[2].location} description = {events[2].description} />
+            {events.map((event, i) => (<Eventcard title ={events[i].title} thumbnail ={events[i].thumbnail} date ={events[i].date} location ={events[i].location} description = {events[i].description} />))}
+
         </div>
-       
-
-
-    );
-  }
+    ); 
+}
 export default EventList;
   
+{/*<Eventcard title ={events[1].title} thumbnail ={events[1].thumbnail} date ={events[1].date} location ={events[1].location} description = {events[1].description} />
+<Eventcard title ={events[2].title} thumbnail ={events[2].thumbnail} date ={events[2].date} location ={events[2].location} description = {events[2].description} /> */}
